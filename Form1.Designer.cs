46ac782a5d5ca08fa6ad3c3415fa5ab13f25982a
Joao Pedro <jp.ssilva@live.com>
2020-05-04 17:46:27 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace _01_Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBaixar = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSite = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBaixar
+            // 
+            this.btnBaixar.Location = new System.Drawing.Point(48, 274);
+            this.btnBaixar.Name = "btnBaixar";
+            this.btnBaixar.Size = new System.Drawing.Size(208, 26);
+            this.btnBaixar.TabIndex = 0;
+            this.btnBaixar.Text = "Fazer Download";
+            this.btnBaixar.UseVisualStyleBackColor = true;
+            this.btnBaixar.Click += new System.EventHandler(this.btnBaixar_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(12, 74);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(292, 182);
+            this.txtResultado.TabIndex = 1;
+            this.txtResultado.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Site:";
+           // this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtSite
+            // 
+            this.txtSite.Location = new System.Drawing.Point(48, 13);
+            this.txtSite.Name = "txtSite";
+            this.txtSite.Size = new System.Drawing.Size(256, 20);
+            this.txtSite.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 312);
+            this.Controls.Add(this.txtSite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnBaixar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaixar;
+        private System.Windows.Forms.RichTextBox txtResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSite;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace _01_Task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Trabalhando de forma Sincrona
+        private void btnBaixar_Click(object sender, EventArgs e)
+        {
+            string endereco = txtSite.Text;
+
+            WebClient web = new WebClient();
+            string html = web.DownloadString(endereco);
+
+            txtResultado.Text = html;
+
+
+        }
+    }
+}
